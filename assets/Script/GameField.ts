cf2c1c6d85977a9fc49760f73e5b1ed92c31bc80
Korsakov Vladislav <korsakov.vlad@gmail.com>
2020-05-18 21:42:45 +0500

    Create letters from prefab on game field

--- assets/Script/GameField.ts
+++ assets/Script/GameField.ts
@@ -1,4 +1,4 @@
-import { Game } from "./common/game";
+import { Game, Cell } from "./common/game";
 
 const {ccclass, property} = cc._decorator;
 
@@ -7,6 +7,9 @@ export default class NewClass extends cc.Component {
 
     @property(Game)
     game: Game = null;
+
+    @property(cc.Prefab)
+    letterPrefab: cc.Prefab = null;
 
     // onLoad () {}
 
@@ -17,6 +20,9 @@ export default class NewClass extends cc.Component {
     startGame () : void {
         this.game = new Game(["мотоцикл","трасса","самолет","сквозняк","шум","ураган","буря","вихрь","торнадо","порыв","вьюга","метель","полёт"]);
         this.game.shuffle();
+        this.game.field.forEach((row : Cell[], y : number) => row.forEach((cell, x) => {
+            const node = cc.instantiate(this.letterPrefab);
+        }))
     }
 
     // update (dt) {}
